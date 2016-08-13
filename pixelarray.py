42ae15068c8ca73789cb5d15275a90b4f588dda5
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-08-12 21:38:17 -0300

    Setting and return data array

--- pixelarray.py
+++ pixelarray.py
@@ -3,6 +3,12 @@ class PixelArray:
     def __init__(self, m, n):
         self.m = m
         self.n = n
+        self._data = []
+        for row in range(m):
+            self._data.append(list([col for col in range(n)]))
 
     def __len__(self):
         return self.m * self.n
+
+    def get_data(self):
+        return self._data
--- tests.py
+++ tests.py
@@ -12,3 +12,18 @@ class PixelArrayTestCase(TestCase):
     def test_must_implement_dunder_len(self):
         obj = PixelArray(5, 5)
         self.assertEqual(len(obj), 25)
+
+    def test_must_have_get_data_function(self):
+        obj = PixelArray(5, 5)
+        self.assertIsNotNone(obj.get_data)
+
+    def test_get_data_must_return_mxn_array(self):
+        m, n = 5, 10
+        obj = PixelArray(m, n)
+        data = obj.get_data()
+
+        self.assertEqual(len(data), m)
+        for row in data:
+            with self.subTest():
+                self.assertEqual(len(row), n)
+
